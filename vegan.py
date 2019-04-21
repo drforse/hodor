e2218b6c23d09aa671a9e367ac77ef6e253025e0
drforse <46328218+drforse@users.noreply.github.com>
2019-04-21 03:58:29 +0100

    updated /main

--- vegan.py
+++ vegan.py
@@ -3,8 +3,9 @@ import telebot
 import emoji
 from emoji import emojize
 from telebot import types
-bot = telebot.TeleBot(os.environ['token'])
-
+#bot = telebot.TeleBot(os.environ['token'])
+bot = telebot.TeleBot('899322145:AAFJHERvFRuCpEsagPeSWpJeDC036-UKDrA')
+#variables
 e = emoji.emojize
 boom = e(':boom:', use_aliases = True)
 energy = e(':zap:', use_aliases = True)
@@ -19,14 +20,17 @@ line = '*______________________________________*'
 spec = "Особенность: "
 group_list = '@mtsgameh\n@LastVegan'
 
+#/help, /start
 @bot.message_handler(commands=['help','start'])
 def send_welcome(message):
     msg = bot.send_message(message.chat.id, 'Привет! Я помогу тебе с поиском информации по игре [VeganWars](t.me/veganwarsbot)!\nСписок команд:\n/weapons - Оружие\n/perks - Способности(перки)\n/items - Предметы\n**\nБот находтся на стадии разработки!!', parse_mode = 'markdown')
 
+#/grouplist
 @bot.message_handler(commands=['grouplist'])
 def send_group_list(message):
     bot.send_message(message.chat.id, "Некоторые из групп, в которых можно поиграть в [VeganWars](t.me/veganwarsbot)\n" + group_list, parse_mode = 'markdown', disable_web_page_preview = True)
 
+#/feedback
 @bot.message_handler(commands=['feedback'])
 def feedback_work(message):
     if message.text in ['/feedback', '/feedback ']:
@@ -35,20 +39,29 @@ def feedback_work(message):
         bot.forward_message(500238135, message.chat.id, message.message_id)
         bot.send_message(message.chat.id, "Спасибо за ваше предложение/отзыв, ваше мнение очень важно нам")
 
+#temp
+@bot.message_handler(commands=['perks', 'items'])
+def send_error(message):
+    bot.send_message(message.chat.id, 'Извините, функция недоступна, т.к. бот находится на стадии разработки')
+
+
+#WEAPONS__________________________________________________________________________________________________________________________________________________!
+#_________________________________________________________________________________________________________________________________________________________!
+
+#Keyboard_lvl_1
 @bot.message_handler(commands=['weapons'])
 def send_weapons(message):
     markup = types.ReplyKeyboardMarkup(row_width=1)
     distant_b = types.KeyboardButton('Оружие дальнего боя')
     close_b = types.KeyboardButton('Оружие ближнего боя')
     super_b = types.KeyboardButton('Наградное оружие')
-    markup.add(distant_b, close_b, super_b)
+    _exit_ = types.KeyboardButton("Выход")
+    markup.add(distant_b, close_b, super_b, _exit_)
     msg = bot.send_message(message.chat.id, 'Выберите тип оружия:', reply_markup=markup)
+    bot.register_next_step_handler(message, send_distant_weapons)
 
-@bot.message_handler(commands=['perks', 'items'])
-def send_error(message):
-    bot.send_message(message.chat.id, 'Извините, функция недоступна, т.к. бот находится на стадии разработки')
 
-@bot.message_handler(content_types=['text'])
+#Keyboard_lvl_2
 def send_distant_weapons(message):
     list = ['Оружие дальнего боя', 'Дробовик', "Револьвер", "Огнемет", "Снайперская винтовка", "Пистолет", "Обрез"]
     list2 = ['Оружие ближнего боя', 'Цепь', 'Факел', 'Копье', 'Топор', 'Нож', 'Полицейская дубинка', 'Бейсбольная бита', 'Кастет', 'Кувалда', 'Булава']
@@ -96,11 +109,15 @@ def send_distant_weapons(message):
         markup_super.add(s0, s1, s2, back, __exit__)
         bot.send_message(message.chat.id, "Выберите оружие:", reply_markup = markup_super)
         bot.register_next_step_handler(message, send_superweapon_info)
+    elif message.text == 'Выход':
+        markup_rem = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Quit. /help', reply_markup = markup_rem)
     elif message.text not in list and message.text not in list2 and message.text not in list3:
         send_weapons(message)
         
+#Nagradnoye oruzhue        
 def send_superweapon_info(message):
-    list = ['Наградное оружие', 'Лук Асгард', 'Катана', 'Копье Нарсил', "/help", "/weapons", "/start", "/perks", "/items", "Назад"]
+    list = ['Наградное оружие', 'Лук Асгард', 'Катана', 'Копье Нарсил', "/help", "/weapons", "/start", "/perks", "/items", "Назад", "/main"]
     if message.text == 'Выход':
         markup_rem = types.ReplyKeyboardRemove()
         bot.send_message(message.chat.id, 'Quit. /help', reply_markup = markup_rem)
@@ -118,8 +135,9 @@ def send_superweapon_info(message):
     elif message.text not in list:
         send_distant_weapons(message)
         
+#Oruzhie dalnego boya        
 def send_weapon_info(message):
-    list = ['Оружие дальнего боя', 'Дробовик', "Револьвер", "Огнемет", "Снайперская винтовка", "Пистолет", "Обрез", "/help", "/weapons", "/start", "/perks", "/items", "Назад"]
+    list = ['Оружие дальнего боя', 'Дробовик', "Револьвер", "Огнемет", "Снайперская винтовка", "Пистолет", "Обрез", "/help", "/weapons", "/start", "/perks", "/items", "Назад", "/main"]
     if message.text == 'Выход':
         markup_rem = types.ReplyKeyboardRemove()
         bot.send_message(message.chat.id, 'Quit. /help', reply_markup = markup_rem)
@@ -146,8 +164,9 @@ def send_weapon_info(message):
     elif message.text not in list:
         send_distant_weapons(message)
 
+#Oruzhie blizhnego boya
 def send_arm_info(message):
-    list = ['Оружие ближнего боя', 'Цепь', 'Факел', 'Копье', 'Топор', 'Нож', 'Полицейская дубинка', 'Бейсбольная бита', 'Кастет', 'Кувалда', 'Булава',"/help", "/weapons", "/start", "/perks", "/items", "Назад"]
+    list = ['Оружие ближнего боя', 'Цепь', 'Факел', 'Копье', 'Топор', 'Нож', 'Полицейская дубинка', 'Бейсбольная бита', 'Кастет', 'Кувалда', 'Булава',"/help", "/weapons", "/start", "/perks", "/items", "Назад", "/main"]
     ur = line + "\n\n" + fist+uron
     en = energy+energia
     ac = accur+accuraci+"`100-99-99-97-93-87-78-65`\n"
@@ -188,6 +207,69 @@ def send_arm_info(message):
         send_weapons(message)
     elif message.text not in list:
         send_distant_weapons(message)
-        
-                
+
+#MAIN_INFO________________________________________________________________________________________________________________________________________________!
+#_________________________________________________________________________________________________________________________________________________________!
+
+@bot.message_handler(commands = ['main'])
+def send_main(message):
+    markup_main = types.ReplyKeyboardMarkup(row_width=1)
+    carac = types.KeyboardButton('Характеристики')
+    do = types.KeyboardButton('Ход')
+    how_to_do = types.KeyboardButton('Последовательность действий')
+    _exit_ = types.KeyboardButton('Выход')
+    markup_main.add(carac, do, how_to_do, _exit_)
+    bot.send_message(message.chat.id, 'Выберите:', reply_markup = markup_main)
+    bot.register_next_step_handler(message, send_keyboard)
+    
+
+def send_keyboard(message):
+    list = ['Характеристики',"Жизнь","Энергия","Урон"]
+    if message.text == 'Ход':
+        send_move(message)
+    elif message.text == 'Последовательность действий':
+        send_order(message)
+    elif message.text == 'Выход':
+        markup_rem = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Quit. /help', reply_markup = markup_rem)
+    elif message.text in list:
+        markup_carac = types.ReplyKeyboardMarkup(row_width = 1)
+        kb_life = types.KeyboardButton('Жизнь')
+        kb_energy = types.KeyboardButton('Энергия')
+        kb_uron = types.KeyboardButton('Урон')
+        back = types.KeyboardButton("Назад")
+        _exit_ = types.KeyboardButton('Выход')
+        markup_carac.add(kb_life, kb_energy, kb_uron, back, _exit_)
+        bot.send_message(message.chat.id, 'Выберите характеристику:', reply_markup = markup_carac)
+        bot.register_next_step_handler(message, send_carac)
+
+def send_carac(message):
+    list = ["Жизнь","Энергия","Урон","Назад","Выход","/help", "/weapons", "/start", "/perks", "/items"]
+    if message.text == 'Жизнь':
+        bot.send_message(message.chat.id, 'Основной параметр, при потере последней жизни вы теряете сознание и не можете продолжать бой (Исключение - Зомби). Можно увеличить стартовое количество жизней с помощью Двужильности, а также восстановить 2 жизни при помощи Стимулятора.')
+        send_keyboard(message)
+    elif message.text == 'Энергия':
+        bot.send_message(message.chat.id, 'От количества энергии зависит точность вашего оружия, а также возможность применения предметов Граната и Коктейль. Расходуется при применении вышеозначенных предметов, а также при использовании оружия.')
+        send_keyboard(message)
+    elif message.text == 'Урон':
+        bot.send_message(message.chat.id, 'Команда, нанесшая больше урона за ход, наносит повреждения, другая не наносит повреждений.\n1-5 урона - отнимает 1 хп\n6-11 урона - отнимает 2 хп\n12-17 урона - отнимает 3 урона, и т.д.')
+        send_keyboard(message)
+    elif message.text == 'Назад':
+        send_main(message)
+    elif message.text == 'Выход':
+        markup_rem = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Quit. /help', reply_markup = markup_rem)
+    elif message.text == 'Ход':
+        send_move(message)
+    elif message.text not in list:
+        send_keyboard(message)
+
+def send_move(message):
+    bot.send_message(message.chat.id, "ХОД" + line + '\n\n' + 'Есть 5 вариантов хода:\n\n*Удар/Выстрел:*\nВы используете свое оружие по назначению! (Нельзя ударить,если соперник не стоит вплотную к вам.)\n\n*Подойти (только для игроков с оружием ближнего боя):*\nВы подходите вплотную ко всем соперникам, после чего вам становится доступен удар оружием ближнего боя(Если у вас оружие ближнего боя, офк).\n\n*Отдышаться:*\nВы восстанавливаете энергию до максимума.\n\n*Пропустить/Потушиться:*\nПропускаете ход. Если вы горите - огонь тушится.\n\n*Предмет:*\nВы используете предмет из числа тех, что лежат в вашем инвентаре.', parse_mode = 'markdown')
+    send_main(message)
+
+def send_order(message):
+    bot.send_message(message.chat.id, 'ПОСЛЕДОВАТЕЛЬНОСТЬ ДЕЙСТВИЙ' + line + '\n\n' '1.Применяются предметы\n\n2.Проводятся выстрелы/удары\n\n3.Те, кто выбрал вариант хода "Подойти", подходят вплотную.\n\n4.Восстанавливается энергия у игроков, выбравших вариант "Отдышаться"\n\n5.Подсчитывается урон, нанесенный обеими командами (включая горение). Все игроки команды, нанесшей меньшее количество урона, теряют X/(6-Y) [X/(10-Y), если взят Крепкий череп](целочисленное деление) +1 жизней, где Х - урон, полученный конкретным игроком (если Х = 0, игрок не теряет жизни).\n\n6.Активируются способности Пироман, Садист, Зомби, Оружейник.(если условие активации выполнено).',parse_mode = 'markdown')
+    send_main(message)
+
 bot.polling()
