473c2c8f9eb4ba59068e084c8603e8dd0a28bfc6
drforse <46328218+drforse@users.noreply.github.com>
2019-05-08 22:47:32 +0100

    Update hodor.py

--- hodor.py
+++ hodor.py
@@ -6,12 +6,12 @@ bot = telebot.TeleBot(os.environ['token'])
 def reply_unic_fucking_func(message):
     list = ['hodor','ходор']
     for keyword in list:
-        if keyword in message.text.lower():
+        if keyword in message.text.lower() or message.reply_to_message.from_user.id == '875106649':
             bot.reply_to(message, 'Hodor')
-@bot.message_handler(content_types = ['text'])
-def reply(message):
-    if message.reply_to_message.from_user.id == '875106649':
-        bot.reply_to(message, 'Hodor')
+#@bot.message_handler(content_types = ['text'])
+#def reply(message):
+#    if message.reply_to_message.from_user.id == '875106649':
+#        bot.reply_to(message, 'Hodor')
         
 
 bot.polling()
