bdc6ab3096931398031d97a2c81aa1cd8bc8c669
drforse <46328218+drforse@users.noreply.github.com>
2019-05-08 22:35:57 +0100

    Update hodor.py

--- hodor.py
+++ hodor.py
@@ -8,6 +8,8 @@ def reply_unic_fucking_func(message):
     for keyword in list:
         if keyword in message.text.lower():
             bot.reply_to(message, 'Hodor')
+@bot.message_handler(content_types = ['text'])
+def reply(message):
     if message.reply_to_message.from_user.id == 875106649:
             bot.reply_to(message, 'Hodor')
         
